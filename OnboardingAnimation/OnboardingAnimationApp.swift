0aec5ea57813dae063acb5ab259dcfdb849f555c
Yohei Okawa <yoppei.louis8c5@gmail.com>
2026-05-29 17:34:38 +0900

    Initial Commit

---
+++ OnboardingAnimation/OnboardingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAnimationApp.swift
+//  OnboardingAnimation
+//
+//  Created by Yohei Okawa on 2026/05/29.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
